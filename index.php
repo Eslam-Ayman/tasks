26503e51a6c2627a29cc5a5508c5388b26056293
eslam <eslam@yahoo.com>
2017-02-20 17:27:40 +0200

    try to chang brand name

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ $location.path()*/
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#home">Tasks</a>
+      <a class="navbar-brand" href="#home">performer</a>
     </div>
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
